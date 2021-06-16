8285057cb5993421802816fb6d60b68d92c06031
Russels-teapot <baxarev.777@yandex.ru>
2021-06-16 14:11:02 +0300

    Частично выполнена домашняя работа

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 
-import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from "./game";
+import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from "./game.js";
 
 const game = new Game();
 game.start();
